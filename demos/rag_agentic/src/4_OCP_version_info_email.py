7a9a69f2969f11ffc55fbe0854f2ba1c4e7efbcc
greg pereira <grpereir@redhat.com>
2025-04-09 10:05:13 -0700

    updating notebook
    Signed-off-by: greg pereira <grpereir@redhat.com>

--- demos/rag_agentic/src/4_OCP_version_info_email.py
+++ demos/rag_agentic/src/4_OCP_version_info_email.py
@@ -25,7 +25,7 @@ logger.addHandler(stream_handler)
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--remote", help="Uses the remote_url", action="store_true")
 parser.add_argument("-s", "--session-info-on-exit", help="Prints agent session info on exit", action="store_true")
-parser.add_argument("-a", "--auto", help="Automatically runs examples, and does not start a chat session", action="store_true")
+parser.add_argument("-c", "--prompt-chaining-mode", help="", action="store_true")
 args = parser.parse_args()
 
 model="meta-llama/Llama-3.2-3B-Instruct"
@@ -59,8 +59,7 @@ agent = Agent(
     You have access to a number of tools.
     Whenever a tool is called, be sure return the Response in a friendly and helpful tone.
     When you are asked to search the web you must use a tool.
-    My team is comprised of the folllowing members, with the corresponding emails: Ryan Cook (rcook@redhat.com), Sally O'Malley (somalley@redhat.com), Tom Coufal (tcoufal@redhat.com), and myself, Greg Pereira (grpereir@redhat.com).
-    When signing off on work, please be sure to include: - Sent from my llama-stack agent in the signature
+    When signing off on emails, please be sure to include: - Sent from my llama-stack agent in the signature
     """,
     tools=["builtin::websearch"],
     tool_config={"tool_choice":"auto"},
@@ -72,23 +71,39 @@ agent = Agent(
 
 session_name = f"Draft_email_with_latest_OCP_version{uuid.uuid4()}"
 session_id = agent.create_session(session_name=session_name)
-prompts = [
-    """Search for the web for the latest Red Hat OpenShift version on the Red Hat website.""",
-    """Summarize the latest Red Hat OpenShift version number and any significant features, fixes, or changes that occure in this version.""",
-    """Draft and format an email to convey this information to my team members."""
-]
 
-for i, prompt in enumerate(prompts):    
+if args.prompt_chaining_mode:
+    prompts = [
+        """Search for the web for the latest Red Hat OpenShift version on the Red Hat website.""",
+        """Summarize the latest Red Hat OpenShift version number and any significant features, fixes, or changes that occure in this version.""",
+        """Draft and format an email to convey this information to my team members."""
+    ]
+    for i, prompt in enumerate(prompts):    
+        turn_response = agent.create_turn(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            session_id=session_id,
+            stream=True,
+        )
+        logger.info(f"========= Turn: {i} =========")
+        for log in EventLogger().log(turn_response):
+            log.print()
+else:
+    prompt = """Search for the web for the latest Red Hat OpenShift version on the Red Hat website. Summarize the version number and draft an email to convey this information."""
     turn_response = agent.create_turn(
         messages=[
             {
-                "role": "user",
-                "content": prompt,
+                "role":"user",
+                "content": prompt
             }
         ],
         session_id=session_id,
         stream=True,
     )
-    logger.info(f"========= Turn: {i} =========")
     for log in EventLogger().log(turn_response):
         log.print()
+
