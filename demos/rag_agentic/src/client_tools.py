7f7c8d9a815a54b3fa60ea0819b0124892fd51e3
Yu An <yuan@redhat.com>
2025-04-23 20:12:11 +0100

    Advance notebook2, prompt chaining with custom location tool and websearch tool

--- demos/rag_agentic/src/client_tools.py
+++ demos/rag_agentic/src/client_tools.py
@@ -1,4 +1,5 @@
 from llama_stack_client.lib.agents.client_tool import client_tool
+import geocoder
 
 @client_tool
 def torchtune(query: str = "torchtune"):
@@ -20,3 +21,20 @@ def torchtune(query: str = "torchtune"):
             Built-in support for many popular dataset formats and prompt templates
     """
     return dummy_response
+
+@client_tool
+def get_location(query: str = "location"):
+    """
+    Provide the location upon request.
+
+    :param query: The query from user
+    :returns: Information about user location
+    """
+    try:
+        g = geocoder.ip('me')
+        if g.ok:
+            return f"Your current location is: {g.city}, {g.state}, {g.country}" # can be modified to return latitude and longitude if needed
+        else:
+            return "Unable to determine your location"
+    except Exception as e:
+        return f"Error getting location: {str(e)}"
